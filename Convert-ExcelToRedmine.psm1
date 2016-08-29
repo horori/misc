7ab05fb897325ad735569fcfc229485a8879a9b1
Hori <SHORI2@bethel.jw.org>
2016-08-29 22:53:51 +0900

    Simply script Covert-ExcelToRedmine.psm1

---
+++ Convert-ExcelToRedmine.psm1
@@ -0,0 +1,28 @@
+﻿function Convert-ExcelToRedmine{ 
+  <# 
+  .SYNOPSIS 
+  Convert tab delimited Excel cells to Redmine wiki table 
+  .DESCRIPTION 
+  Simply replace tab to "|" charactor between the cells.
+  .EXAMPLE 
+  Convert-ExcelToRedmine @"
+  <PASTE EXCEL CELLS HERE>
+  "@
+  .PARAMETER computername 
+  The computer name to query. Just one. 
+  .PARAMETER logname 
+  The name of a file to write failed computer names to. Defaults to errors.txt. 
+  #> 
+  [CmdletBinding()] 
+  param 
+  ( 
+    [Parameter(Mandatory=$True, 
+    ValueFromPipeline=$True)] 
+    [string]$cells
+  ) 
+  process { 
+    $cells.Split("`n")|?{$_.Length-gt1}|%{"| $($_-replace"`t"," | ") |"}
+  } 
+}
+
+Export-ModuleMember -Function Convert-ExcelToRedmine
