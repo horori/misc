2f06d435f17b22c7a673a06fefe673ecfd5c0471
shori <shori@bethel.jw.org>
2017-02-03 08:46:22 +0900

    Refined the folder structure

---
+++ Redmine/Trace-RedmineProductionLog.ps1
@@ -0,0 +1,49 @@
+﻿<#
+.SYNOPSIS
+Parse Redmine ./log/production.log on Windows Client
+.DESCRIPTION
+Parse Redmine ./log/production.log on Windows Client
+.EXAMPLE
+.\Trace-RedmineProductionLog.ps1 | Export-CSV -NoTypeInformation "$env:USERPROFILE\Desktop\Result.csv"
+#>
+param(
+    [Parameter(Mandatory=$True, 
+        HelpMessage='Path to production.log from Redmine')] 
+    [string]$LogFile
+)
+Process{
+    $current = $null
+    Get-Content $LogFile | ForEach-Object{
+        $line = $_
+
+        #$line = 'Started GET "/redmine/projects/procurement/issues" for 127.0.0.1 at 2016-09-27 13:10:10 +0900'
+        #$line = 'Completed 200 OK in 406.2ms (Views: 140.6ms | ActiveRecord: 140.6ms)'
+
+        if( $line -match "^Started " ){
+            if( $current ){
+                Write-Output $current
+            }
+            $array = $line -split " "
+            $current = [PSCustomObject]@{
+                Type = $array[1]
+                Query = $array[2] -replace '"'
+                For = $array[4]
+                Date = $array[6] + " " + $array[7]
+                Offset = $array[8]
+                Status = ""
+                ProcessTime = ""
+            }
+        }
+
+        if( $line -match "^Completed" ){
+            $array = $line -split " "
+            if( $current ){
+                $current.Status = $array[1]
+                $current.ProcessTime = $array[4]
+                Write-Output $current
+            }else{
+
+            }
+        }
+    }
+}
---
+++ Redmine/Trace-RedmineUPHelpDeskLog.ps1
@@ -0,0 +1,32 @@
+﻿<#
+.SYNOPSIS
+Sample code to count how many e-mails received per day from redmine_helpdesk log on desktop.
+.DESCRIPTION
+Sample code to count how many e-mails received per day from redmine_helpdesk log on desktop.
+https://www.redmineup.com/pages/plugins/helpdesk
+.EXAMPLE
+.\Trace-RedmineUPHelpDeskLog.ps1 | Export-CSV -NoTypeInformation "$env:USERPROFILE\Desktop\Result.csv"
+#>
+param(
+    [Parameter(Mandatory=$True, 
+        HelpMessage='Path to redmine_helpdesk.log from RedmineUP HelpDesk Plugin')] 
+    [string]$LogFile
+)
+Process{
+    $current = $null
+    $i = 0
+
+    Get-Content $LogFile | ForEach-Object{
+        $logLine = $_
+        if( $logLine -match "^\[INFO\] - " ){
+            $date = $logLine.Substring(9,6) # Get '22 Jan'
+            if( $date -eq $current ){
+                $i++
+            }else{
+                Write-Output "$current, $i" # Output '22 Jan, <E-mail Count>'
+                $i = 1
+                $current = $date
+            }
+        }
+    }
+}
